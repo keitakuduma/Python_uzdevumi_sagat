34dc1267b8dd46eda70bea8f6d95a115d1f11575
keitakuduma <keita.kuduma@grobinasskola.lv>
2023-11-03 10:49:38 +0200

    2.uzd

--- 2uzd.py
+++ 2uzd.py
@@ -1,4 +1,25 @@
 """ 
 Iegūt ziņas, izmantojot RSS plūsmu no google.lv.
+1) Kas ir RSS? REALLY SIMPLE SYNDICATION
+jaunumi un saturs, satura agregatori(abonēšana)
+2) plūsmu no google.lv
+pats par sevi ir vietne, bet nav satura radītāja. google izmanto RSS.
+
+
+pip install feedparser
+...pip upgrade ... 
 
 """
+import feedparser
+
+#URL uz RSS plūsmu 
+rss_url='https://news.google.com/home?hl=en-LV&gl=LV&ceid=LV:en'
+# iegūstam RSS plūsmas datus un veicam analizi
+kkk=feedparser.parse(rss_url)
+
+#noformēšana
+for entry in kkk.entries:
+    print("Virsraksts", entry.title)
+    print("Saite", entry.link)
+    #print("Publicēšanas datums", entry.published)
+    print("\n")
